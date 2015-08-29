4be278a39413ad21499b1bffb1266facf4b4cb74
moe-ped <m.berning@web.de>
2015-08-29 13:00:29 +0200

    :(

---
+++ Assets/Scripts/DraggableObject.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class DraggableObject : MonoBehaviour, IDragHandler, IBeginDragHandler, IPointerClickHandler 
+{
+	#region IPointerClickHandler implementation
+
+	public void OnPointerClick (PointerEventData eventData)
+	{
+		Debug.Log ("starting drag");
+	}
+
+	#endregion
+
+	#region IBeginDragHandler implementation
+
+	public void OnBeginDrag (PointerEventData eventData)
+	{
+		Debug.Log ("starting drag");
+	}
+
+	#endregion
+
+	#region IDragHandler implementation
+
+	public void OnDrag (PointerEventData eventData)
+	{
+		Debug.Log ("being dragged");
+		transform.position = Input.mousePosition;
+	}
+
+	#endregion
+}
