407123f23a2ad7d799874d4302dd7267da982f1f
ldaldrup <ldaldrup95@gmail.com>
2015-08-30 23:57:13 +0200

    Added sound option
    -added sound options to the settings menu
    
    -settings are cached and are used when the client is reloaded
    
    added load scene to load persistent scripts
    
    tweaked main menu layout

---
+++ Assets/Scripts/LoadManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Application.GetStreamProgressForLevel("menu") == 1) {
+			Application.LoadLevel ("menu");
+		}
+	}
+}
--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class MenuManager : MonoBehaviour {
@@ -8,10 +9,22 @@ public class MenuManager : MonoBehaviour {
 	public GameObject MenuBtnPanel;
 	public GameObject ConfirmPanel;
 	public GameObject ComingSoonPanel;
+	public GameObject SettingsPanel;
+	public GameObject MenuAudio;
+
+	private AudioSource[] audioSources;
+	private AudioSource menuMusic;
+	private AudioSource btnSound;
 
 	// Use this for initialization
-	void Start () {
-	
+	void Awake () {
+		audioSources = MenuAudio.GetComponents<AudioSource>();
+		btnSound = audioSources[1];
+		menuMusic = audioSources[0];
+	}
+
+	void Start(){
+		menuMusic.Play();
 	}
 	
 	// Update is called once per frame
@@ -20,34 +33,66 @@ public class MenuManager : MonoBehaviour {
 	}
 
 	public void changeMenu (GameObject btn) {
+		
+		btnSound.Play();
+
+		// load main level when clicking the play btn
 		if (btn.gameObject.name == "PlayBtn") {
 			Debug.Log ("PlayBtn pressed");
 			Application.LoadLevel ("Main");
-		} else if (btn.gameObject.name == "HighscoreBtn") {
+		} 
+
+		// Show the highscore panel when clicking the highscore btn
+		else if (btn.gameObject.name == "HighscoreBtn") {
 			Debug.Log ("HighscoreBtn pressed");
 			MenuBtnPanel.SetActive (false);
 			ComingSoonPanel.SetActive (true);
-		} else if (btn.gameObject.name == "SettingsBtn") {
+		} 
+
+		// Show settings panel when clicking the options btn
+		else if (btn.gameObject.name == "SettingsBtn") {
 			Debug.Log ("SettingsBtn pressed");
 			MenuBtnPanel.SetActive (false);
-			ComingSoonPanel.SetActive (true);
-		} else if (btn.gameObject.name == "ExitBtn") {
+			SettingsPanel.SetActive (true);
+			GameObject.Find("VolumeSlider").GetComponent<Slider>().value = AudioListener.volume;
+		} 
+
+		// Show exit confirmation panel when clicking the exit btn
+		else if (btn.gameObject.name == "ExitBtn") {
 			Debug.Log ("Exit Btn pressed");
 			MenuBtnPanel.SetActive (false);
 			ConfirmPanel.SetActive (true);
-		} else if (btn.gameObject.name == "YesBtn") {
+		} 
+
+		// Close game when player confirms the confirmation panel via the Yes btn
+		else if (btn.gameObject.name == "YesBtn") {
 			Debug.Log ("Exit confirmed");
 			Application.Quit ();
-		} else if (btn.gameObject.name == "NoBtn") {
+		} 
+
+		// Abort the game exit when the player clicks the No btn
+		else if (btn.gameObject.name == "NoBtn") {
 			Debug.Log ("Exit aborted");
 			ConfirmPanel.SetActive (false);
 			MenuBtnPanel.SetActive (true);
-		} else if (btn.gameObject.name == "MainMenuBtn") {
+		} 
+
+		// Set cursor to visible and go to main menu when clicking the main menu button ingame
+		// TODO: - this should not be part of the MenuManager script LOL, 
+		else if (btn.gameObject.name == "MainMenuBtn") {
 			Cursor.visible = true;
 			Application.LoadLevel (0);
-		} else if (btn.gameObject.name == "BackBtn") {
+		} 
+
+		// Disable the coming soon panel and show the Menu panel
+		else if (btn.gameObject.name == "BackBtn") {
 			ComingSoonPanel.SetActive (false);
+			SettingsPanel.SetActive (false);
 			MenuBtnPanel.SetActive (true);
+		}
+
+		else if (btn.gameObject.name == "ApplyBtn") {
+			PlayerPrefs.Save();
 		}
 	}
 }
