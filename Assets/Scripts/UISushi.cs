447a1a171e1d2387f7fb75ffdb7ff8b64526c694
moe-ped <m.berning@web.de>
2015-08-31 20:03:00 +0200

    Added lost files

---
+++ Assets/Scripts/UISushi.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class UISushi : MonoBehaviour 
+{
+    public Action<int> GotDestroyed = (int Type) => { };
+    public int Type;
+
+    void OnDestroy()
+    {
+        GotDestroyed(Type);
+    }
+}
